75b054a0d40a4073cd790e55b1e8a86ecdf6b9b8
Mohammed Yaqob <mohammed.yaqob.98@gmail.com>
2021-08-31 15:49:53 +0200

    Initial Commit

---
+++ FlashChat/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  FlashChat
+//
+//  Created by Mohammed Yaqob's MacBook on 15/08/2020.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
